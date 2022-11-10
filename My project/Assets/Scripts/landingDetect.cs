f29f6d1c7be3e4d81b57c84d006cb31f3a7c1e92
ShuaiYuan <42807937+Hanyetu@users.noreply.github.com>
2022-11-10 04:42:28 -0500

    V3.1

---
+++ My project/Assets/Scripts/landingDetect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landingDetect : MonoBehaviour
+{
+    public bool landingState = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        landingState = true;
+    }
+}
